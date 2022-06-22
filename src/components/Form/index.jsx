6f639d1b69a99d2772ed94ceccbb7d81e56b36ba
Victor Hugo Garcia de Farias <vhfarias_1@hotmail.com>
2022-06-21 21:46:10 -0300

    fixed Form CSS

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -9,7 +9,6 @@ export const Form = ({ setEntries, config }) => {
 
   const [formData, handleChange, handleSubmit, errors] = useForm(config, setEntries);
 
-
   let fields = Object.entries(config).map(([name, settings], index) => {
     let element;
     switch (settings.type) {
@@ -51,46 +50,6 @@ export const Form = ({ setEntries, config }) => {
       >
         Novo Item
       </button>
-
-      {/* <div>
-        <FormInput
-          name="description"
-          type="text"
-          title="Descrição"
-          value={formData.description}
-          onChange={handleChange('description')}
-          errors={errors}
-        />
-        <FormInput
-          name="amount"
-          type="text"
-          title="Valor (R$)"
-          value={formData.amount}
-          onChange={handleChange('amount')}
-          errors={errors}
-        />
-        <FormInput
-          name="reference"
-          type="text"
-          title="Referência"
-          value={formData.reference}
-          onChange={handleChange('reference')}
-          errors={errors}
-        />
-      </div>
-      <div>
-        <FormRadioGroup
-          optionsList={['Entrada', 'Saída']}
-          value={formData.type}
-          onChange={handleChange('type')}
-          errors={errors}
-        />
-        <button
-          formAction='submit'
-        >
-          Novo Item
-        </button>
-      </div> */}
     </form>
   )
 }
